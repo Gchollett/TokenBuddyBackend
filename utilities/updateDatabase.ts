7c5b1378c83909461d4bf2ae4d573deefa4f03f2
garrett <garrettchollett@yahoo.com>
2023-09-27 00:28:36 -0500

    Finished update database script; Got all tokens into database!

--- utilities/updateDatabase.ts
+++ utilities/updateDatabase.ts
@@ -1,31 +1,31 @@
 import addCards from "./add-cards"
 import clearDatabase from "./clear-database"
 
-type card = {
-  name: string,
-  power: string,
-  toughness: string,
-  frontImage: string,
-  backImage: string
+const fetchData = (page) => {
+  fetch(page).then(async res => {
+    res.json().then(async resJson => {
+      const cards = resJson.data.map(card => {
+        return {
+          name: card.name,
+          power: card.power,
+          toughness: card.toughness,
+          frontImage: card.image_uris?.border_crop,
+          backImage: "hello"
+        }
+      })
+      await addCards(cards);
+      if(resJson.has_more){
+        fetchData(resJson.next_page);
+      }
+    })
+  })
 }
 
 // https://api.scryfall.com/cards/search?q=t%3Atoken
 const main = async () => {
-  fetch('https://api.scryfall.com/cards/search?q=t%3Atoken&unique=cards')
-  .then(async res => {
-    res.json().then(scryfallCards =>{
-    const cards : card[] = scryfallCards.data.map(card => {
-      return {
-        name: card.name,
-        power: card.power,
-        toughness: card.toughness,
-        frontImage: card.image_uris?.border_crop,
-        backImage: "hello"
-      }
-    })
-    clearDatabase();
-    addCards(cards);
-  })})
+  const apiUrl = 'https://api.scryfall.com/cards/search?q=t%3Atoken&unique=cards';
+  await clearDatabase();
+  await fetchData(apiUrl);
 }
 
 main();
