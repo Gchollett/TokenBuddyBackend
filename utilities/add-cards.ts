9c9f120d8c1ecefa84e45b1e49ba5e48acc57ebd
garrett <garrettchollett@yahoo.com>
2023-12-15 00:11:11 -0600

    Work in progress

--- utilities/add-cards.ts
+++ utilities/add-cards.ts
@@ -1,16 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
-type card = {
-    name: string,
-    power: string,
-    toughness: string,
-    frontImage: string,
-    backImage: string
-}
 
-type battlefield = card[]
-
-const addCards = async (cards : battlefield) => {
+const addCards = async (cards) => {
     cards.forEach(async card => {
         const transaction = await prisma.card.create({data: {...card}})
         console.log(transaction)
--- utilities/updateDatabase.ts
+++ utilities/updateDatabase.ts
@@ -5,12 +5,39 @@ const fetchData = (page) => {
   fetch(page).then(async res => {
     res.json().then(async resJson => {
       const cards = resJson.data.map(card => {
+        if(card.card_faces){
+          const front = card.card_faces[0]
+          const back = card.card_faces[1]
+          return {
+            name: card.name,
+            multiFaced: true,
+            faces: {
+              createMany:{
+                data: [
+                  {
+                    name: front.name,
+                    power: front.power,
+                    toughness: front.toughness,
+                    image: front.image_uris?.border_crop
+                  },
+                  {
+                    name: back.name,
+                    power: back.power,
+                    toughness: back.toughness,
+                    image: back.image_uris?.border_crop
+                  }
+                ]
+              }    
+            }   
+          }
+        }
         return {
           name: card.name,
+          multiFaced: false,
+          faces: [],
           power: card.power,
           toughness: card.toughness,
-          frontImage: card.image_uris?.border_crop,
-          backImage: "hello"
+          image: card.image_uris?.border_crop
         }
       })
       await addCards(cards);
@@ -26,6 +53,7 @@ const main = async () => {
   const apiUrl = 'https://api.scryfall.com/cards/search?q=t%3Atoken&unique=cards';
   await clearDatabase();
   await fetchData(apiUrl);
+  console.log('Database Updated');
 }
 
 main();
